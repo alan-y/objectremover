1e5b99d9b23bf782df72ff972f6d62c7326fcbfa
unknown <alanye01@nss.scot.nhs.uk>
2019-02-14 12:18:08 +0000

    new feature: select object type

--- R/object_remove.R
+++ R/object_remove.R
@@ -3,15 +3,22 @@
 object_remove <- function() {
 
     ui <- miniUI::miniPage(
-        miniUI::gadgetTitleBar("Clean Workspace"),
+        miniUI::gadgetTitleBar("Object Remover"),
         miniUI::miniContentPanel(
             # Define layout, inputs, outputs
             shiny::radioButtons("pattern", "Remove Objects",
-                         c("Starting With" = "starting with",
-                           "Ending With" = "ending with",
-                           "According to Regex Pattern" = "according to the regex pattern")),
+                                c("Starting With" = "starting with",
+                                  "Ending With" = "ending with",
+                                  "According to Regex Pattern" = "according to the regex pattern")),
 
             shiny::textInput("txt", "Text Pattern", "z"),
+
+            shiny::checkboxGroupInput("checkGroup",
+                                      label = shiny::strong("Object Types"),
+                                      choices = list("Data Frame" = 1L,
+                                                     "Function" = 2L,
+                                                     "Other" = 3L),
+                                      selected = 1L:3L),
 
             shiny::strong("Objects to be removed"),
             shiny::verbatimTextOutput("objects")
@@ -28,18 +35,40 @@ object_remove <- function() {
             } else {
                 input$txt
             }
+        })
 
+        reactiveObj <- shiny::reactive({
+            obj_txt <- ls(pattern = reactivePattern(), envir = globalenv())
+            obj <- lapply(.GlobalEnv, class)[obj_txt]
+
+            if (!1L %in% input$checkGroup) {
+                obj <- obj[!grepl("data\\.frame", obj)]
+            }
+
+            if (!2L %in% input$checkGroup) {
+                obj <- obj[!grepl("function", obj)]
+            }
+
+            if (!3L %in% input$checkGroup) {
+                obj <- obj[grepl("function|data\\.frame", obj)]
+            }
+
+            names(obj)
         })
 
         output$objects <- shiny::renderPrint({
-            ls(pattern = reactivePattern(), envir = globalenv())
+            reactiveObj()
         })
 
         # When the Done button is clicked, return a value
         shiny::observeEvent(input$done, {
-            rm(list = ls(pattern = reactivePattern(), envir = globalenv()),
-               envir = globalenv())
-            shiny::stopApp(message("Removed objects ", input$pattern, " '", input$txt, "'"))
+            rm(list = reactiveObj(), envir = globalenv())
+            shiny::stopApp(message(
+                "Removed objects (",
+                paste(c("Data Frame", "Function", "Other")[as.integer(input$checkGroup)],
+                      collapse = ", "),
+                ") ", input$pattern, " '", input$txt, "'")
+            )
         })
 
         shiny::observeEvent(input$cancel, {
